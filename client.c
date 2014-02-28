ab8d73e90523d9cc9af09adc296ea23a92db7442
letsspeak <letsinfinite@gmail.com>
2014-02-28 20:26:39 +0900

    connect to hostname

--- client.c
+++ client.c
@@ -3,18 +3,31 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-int main()
+int main(int argc, char **argv)
 {
+  struct hostent *host;
   struct sockaddr_in server;
   int sock;
   char buf[32];
   int n;
 
+  if (argc != 2) {
+    fprintf(stderr, "USAGE: ./client <host>");
+    exit(1);
+  }
+
+  if ((host = gethostbyname(argv[1])) == NULL) {
+    fprintf(stderr, "(mini) nslookup failed on '%s'\n", argv[1]);
+    exit(1);
+  }
+
   sock = socket(AF_INET, SOCK_STREAM, 0);
 
   server.sin_family = AF_INET;
   server.sin_port = htons(12345);
-  server.sin_addr.s_addr = inet_addr("127.0.0.1");
+  server.sin_addr.s_addr = *((unsigned long*) host->h_addr_list[0]);
+  printf("connecting %s (%s)...\n", argv[1], inet_ntoa(server.sin_addr));
+//  server.sin_addr.s_addr = inet_addr("127.0.0.1");
 
   connect(sock, (struct sockaddr*)&server, sizeof(server));
 
